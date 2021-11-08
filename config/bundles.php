6f640720de13130af501d396f37bf5e59db56914
jgcohen <75163282+jgcohen@users.noreply.github.com>
2021-11-08 09:29:51 +0100

    adding httpclient and  symfonyUx

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
 ];
